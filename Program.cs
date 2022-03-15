a60c10926c9a0a538f4c4bbe98177f2079090d23
slu2023 <slu2023@chadwickschool.org>
2022-03-15 09:51:23 -0700

    chore: Initialize project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MY_AP_PROJECT_X_X
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
