12660de957aa95cdde0f54659a1bbc5f1216db78
slu2023 <slu2023@chadwickschool.org>
2022-04-12 10:36:16 -0700

    FINISHED!!!! SO PROUD OF MYSELF!!! THANKY SOO MUCH

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MY_AP_PROJECT_X_X
 {
     class Program
     {
+
+        private static object guess;
+        private static object correctWord;
+
         static void Main(string[] args)
         {
             // Console.ForegroundColor = ConsoleColor.DarkBlue;
@@ -13,18 +18,31 @@ namespace MY_AP_PROJECT_X_X
             if (args.Length > 0 && args[0] == "test")
             {
                 TestAll();
-                return; // Exits the program
+                return;
             }
+            string correctWord;
+            string guess;
+            correctWord = TheWord();
+
+            do
+            {
+                guess = UserInput(correctWord);
+                Info(guess, correctWord);
+                Console.WriteLine();
+            } while (guess != correctWord);
+            // return UserInput(correctWord);
         }
+
+
 
         public static void TestAll()
         {
-            string testTheWord = TestTheWord.RunTest();
-            Console.WriteLine($"Test TheWord(words);{testTheWord}");
-            string testUserInput = TestUserInput.RunTest();
-            Console.WriteLine($"Test UserInput(words);{testUserInput}");
-            bool testInfo = TestInfo.RunTest();
-            Console.WriteLine($"Test Info(words);{testInfo}");
+            // string testTheWord = TestTheWord.RunTest();
+            // Console.WriteLine($"Test TheWord(words);{testTheWord}");
+            // string testUserInput = TestUserInput.RunTest();
+            // Console.WriteLine($"Test UserInput(words);{testUserInput}");
+            // bool testInfo = TestInfo.RunTest();
+            // Console.WriteLine($"Test Info(words);{testInfo}");
         }
 
 
@@ -32,7 +50,7 @@ namespace MY_AP_PROJECT_X_X
         /// Generates a word for the player to guess
         /// </summary>
         /// <returns>the random word that was generated</returns>
-    
+
         public static string TheWord()
         {
 
@@ -41,7 +59,25 @@ namespace MY_AP_PROJECT_X_X
             // 3. Generate a random number between 0 and `words.Count` and store the result in
             //    a variable called `ix`
             // 4. Return the word at position `ix`. (e.g. `words[ix]`)
-            return null;
+
+            List<string> words = new List<string>();
+
+            words.Add("ice-cream");
+            words.Add("sandwich");
+            words.Add("burger");
+            words.Add("running");
+            words.Add("basketball");
+
+            Random generator = new Random();
+            int index = generator.Next(0, words.Count);
+
+            string word = words[index];
+            int length = word.Length;
+            string randomWord = words[index];
+            Console.WriteLine("Your random word is generated!");
+            Console.WriteLine($"It contains {randomWord.Length} letters");
+
+            return randomWord;
         }
 
         /// <summary>
@@ -53,22 +89,21 @@ namespace MY_AP_PROJECT_X_X
         /// </summary>
         /// <param name="prompt">The message to display to the user</param>
         /// <returns>Valid User Input</returns>
-        public static string UserInput(string prompt)
+        public static string UserInput(string word)
+        
         {
-            return null;
+            Console.Write("Your guess:  ");
+            string guess;
+            guess = Console.ReadLine();
+            if (guess.Length != word.Length)
+            {
+                Console.Error.WriteLine($"Your guess has to contains {word.Length} letters!");
+                return guess;
+            }
+
+            return guess;
         }
 
-        public static string UserInput()
-        {
-
-            // 1. Count the number of letters in the secret word.
-            // 2. Store the number of letters in the secret word in Word.Length.
-            // 3. Prompt the user to make a guess
-            // 4. Read input from the keyboard and store the results in a variable named guess.
-            // 5. If guess is the correct length, return the guess.
-            // 6. If guess is the incorrect length, display the message that guess must contains x letters.
-            return null;
-        }
 
         /// <summary>
         /// Check if the user guessed the word
@@ -78,9 +113,45 @@ namespace MY_AP_PROJECT_X_X
         /// </summary>
         /// <param name="prompt">The message to display to the user</param>
         /// <returns>The positive number the user chose</returns>
-        public static void Info(string guess, string correct)
+        public static bool Info(string guess, string correct)
         {
+            // string word = "basketball";
+            int length = correct.Length;
+            // Console.WriteLine("Your guess:  ");
+            // guess = Console.ReadLine();
+            if (guess == correct)
+            {
+                Console.Error.WriteLine($"The word is {correct}");
+            }
+            else
+            {
+                int pos = 0;
 
+                foreach (char letter in guess)
+                {
+                    if (correct.Contains(letter) == true)
+                    {
+                        if (letter == correct[pos])
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        }
+                    }
+
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                    }
+                    Console.Write(letter);
+                    pos = pos + 1;
+
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.Black;
+            return false;
         }
 
         public static void Info(char letter, int pos, string correct)
